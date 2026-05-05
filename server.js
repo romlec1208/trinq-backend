3e639408024600a5beda2e29d7ba49577112b439
romlec1208 <romlec1208@gmail.com>
2026-05-05 21:02:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -102,4 +102,5 @@ app.get('/api/debug-db', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`🚀 Serveur TRINQ démarré sur le port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Serveur TRINQ démarré sur le port ${PORT}`));
+
