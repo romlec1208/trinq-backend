9a86099a263dbcc473e5e33babc8532b633405a5
romlec1208 <romlec1208@gmail.com>
2026-05-06 21:28:18 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['https://trinq.be', 'http://localhost:3000'],
+        origin: ['https://trinq.be', 'http://localhost:3000', 'https://melodic-creponne-95434f.netlify.app'],
         methods: ['GET', 'POST']
     }
 });
